fba535eb1e7130293bbd7ada3330689a66fc1bdb
ZhiFeng Hu <hufeng1987@gmail.com>
2015-03-23 21:04:23 +0800

    Add anorm example for admin

--- app/controllers/Admin.scala
+++ app/controllers/Admin.scala
@@ -2,6 +2,10 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import anorm._
+import play.api.db.DB
+import play.api.Play.current
+
 
 object Admin extends Controller {
 
@@ -10,7 +14,10 @@ object Admin extends Controller {
   }
 
   def login = Action {
-    Ok("admin/login")
+    DB.withConnection { implicit c =>
+      val result: Boolean = SQL("Select 1").execute()
+      Ok("admin/login" + result)
+    }
   }
 
 }
